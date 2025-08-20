15e1eaf43dffb7547e964e0d0548e771477b7e7c
Himanshu Kumar Singh <R10651@apexuniversity.co.in>
2025-08-20 14:51:35 +0530

    Modules

---
+++ Java Script/Module/Test/index.js
@@ -0,0 +1,8 @@
+const math = require("../math");
+const greet = require("../greet");
+
+console.log("Sum of 2 and 3 = ",math.add(2,3));
+console.log("Minus of 5 and 2 = ",math.subs(5,2));
+
+console.log(greet("Himanshu"));
+console.log(greet("Kapil"));
